1de4d186618c8f8f3af359e55f8cc4e40da96516
balckowl <shiggyjr1492@gmail.com>
2024-07-13 18:07:07 +0900

    fix: correct typos and switch to PascalCase

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import notFoundImg from "@/assets/404-visual.png";
+import NotFoundImg from "@/assets/404-visual.png";
 import Footer from "@/components/layouts/footer";
 import Header from "@/components/layouts/header/header";
 
@@ -9,8 +9,8 @@ export default function NotFound() {
     <>
       <Header />
       <div className="min-h-[calc(100vh-60px-60px)] pt-[40px]">
-        <Image src={notFoundImg} alt="" className="mx-auto mb-[10px]" />
-        <p className="text-center text-[2.0rem] font-bold">tipsもページないよ</p>
+        <Image src={NotFoundImg} alt="" className="mx-auto mb-[10px]" />
+        <p className="text-center text-[2.0rem] font-bold">tipsもページもないよ</p>
       </div>
       <Footer />
     </>
